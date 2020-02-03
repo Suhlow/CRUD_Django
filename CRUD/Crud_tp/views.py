dd78aeb640deae4fd6ca5e280f2c3954f7be9ae1
Mehcheh <macha.jumelin@epsi.fr>
2020-02-03 15:07:53 +0100

    add: features : admin full ok
    bis - because i suck at git

--- CRUD/Crud_tp/views.py
+++ CRUD/Crud_tp/views.py
@@ -3,17 +3,19 @@ from .forms import PostForm
 from .models import Post
 from django.views.generic import ListView, DetailView
 
-#home view for posts. Posts are displayed in a list
+#READ
 class IndexView(ListView):
  template_name='Crud_tp/index.html'
  context_object_name = 'post_list'
  def get_queryset(self):
   return Post.objects.all()
-#Detail view (view post detail)
+
+#READ(DETAIL)
 class PostDetailView(DetailView):
  model=Post
  template_name = 'Crud_tp/post-detail.html'
-#New post view (Create new post)
+
+#CREATE
 def postview(request):
  if request.method == 'POST':
   form = PostForm(request.POST)
@@ -22,7 +24,8 @@ def postview(request):
   return redirect('index')
  form = PostForm()
  return render(request,'Crud_tp/post.html',{'form': form})
-#Edit a post
+
+#EDIT
 def edit(request, pk, template_name='Crud_tp/edit.html'):
     post= get_object_or_404(Post, pk=pk)
     form = PostForm(request.POST or None, instance=post)
@@ -30,7 +33,8 @@ def edit(request, pk, template_name='Crud_tp/edit.html'):
         form.save()
         return redirect('index')
     return render(request, template_name, {'form':form})
-#Delete post
+
+#DELETE
 def delete(request, pk, template_name='Crud_tp/confirm_delete.html'):
     post= get_object_or_404(Post, pk=pk)
     if request.method=='POST':
